901b47a42e84a3a38c6d5318c1220a47e23dea47
Rosas Behoundja <perrierosas@gmail.com>
2025-04-28 14:39:14 +0100

    feat: add perceptron implementation with torch

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,21 @@
+# - Scratch
 import numpy as np
 import random
+from activation_function import step
+# - Pytorch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+# - Tensorflow
 
-def step(z):
-    return np.where(z >= 0, 1, 0)
+
+###########################################
+# From scratch perceptron implementation
 
 class Perceptron:
     """
     A perceptron is an algorithm for supervised learning of
-    binary classifier.
+    linear binary classifier.
     """
     def __init__(self, lrnRate: float, nIter: int) -> None:
         self.lr = lrnRate
@@ -15,7 +23,7 @@ class Perceptron:
         self.w = None
         self.b = None
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+    def fit(self, X: np.ndarray, y: np.ndarray, threshold: float = 0) -> None:
         """Fit the model"""
         _, n_features = X.shape
         self.w = np.random.randn(n_features)
@@ -23,44 +31,63 @@ class Perceptron:
 
         for _ in range(self.nIter):
             for xi, yi in zip(X, y):
-                z = np.dot(xi, self.w) + self.b
-                y_pred = step(z)
+                y_pred = self.forward(xi, threshold)
                 error = yi - y_pred
                 self.w += self.lr * error * xi
                 self.b += self.lr * error
 
         return self
     
-    def forward(self, X: np.ndarray):
+    def forward(self, X: np.ndarray, threshold: float = 0):
         """Computes the linear combination of x1, x2, ... , xn and applies the activation function"""
         z = np.dot(X, self.w) + self.b
-        return z
+        y_pred = step(z, threshold)
+        return y_pred
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        z = self.forward(X)
-        return step(z)
-
-if __name__ == "__main__":
-    X = np.array([
-        [0, 0],
-        [0, 1],
-        [1, 0],
-        [1, 1]
-    ])
-    y = np.array([0, 1, 1, 1])  # OR: sortie attendue
-
-    perceptron = Perceptron(lrnRate=0.1, nIter=50)
-    perceptron.fit(X, y)
-    predictions = perceptron.predict(X)
+    def predict(self, X: np.ndarray, threshold: float = 0) -> np.ndarray:
+        return self.forward(X, threshold)
     
-    # Affichage
-    print("Entrées :")
-    print(X)
-    print("\nVraies sorties :")
-    print(y)
-    print("\nPrédictions du perceptron :")
-    print(predictions)
-    
-    accuracy = np.mean(predictions == y)
-    print(f"\nTaux de précision : {accuracy * 100:.2f}%")
+##############################################
+# Perceptron implementation using pytorch
 
+class pyPerceptron(nn.Module):
+    def __init__(self, input_size):
+        super(pyPerceptron, self).__init__()
+        self.linear = nn.Linear(input_size, 1) # couche d'entrée
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, X):
+        linear_output = self.linear(X)
+        prediction = self.sigmoid(linear_output)
+        return prediction
+    
+def train(X: torch.Tensor, y: torch.Tensor, lr: float = 0.1, epochs: int = 1000):
+    """Fit the model"""
+    input_dim = X.size(dim=1)
+    # model initialization
+    model = pyPerceptron(input_dim)
+    criterion = nn.BCELoss()
+    optimizer = optim.SGD(model.parameters(), lr)
+
+    for epoch in range(epochs):
+        # initialisation de tous les gradients
+        optimizer.zero_grad()
+        predictions = model(X)
+        loss = criterion(predictions, y.unsqueeze(1))
+        # Calcule des gradients
+        loss.backward()
+        # Mise à jour des poids/biais
+        optimizer.step()
+
+        if (epoch + 1) % 100 == 0:
+            print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+    return model
+
+##############################################
+# Perceptron implementation using tensorflow
+
+class tfPerceptron:
+
+    def __init__(self):
+        pass
